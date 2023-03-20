543fe604557c213619cf7f795931f4b3e3b6ed0c
DimaProhorenko <dima.study.web@gmail.com>
2023-03-20 02:04:21 +0200

    features section and change header on scroll

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,18 @@
 const burgerBtn = document.querySelector('.burger');
 const navEl = document.querySelector('.header__nav');
-console.log(burgerBtn);
+const headerEl = document.querySelector('.header');
+const maxScroll = 200;
 
 burgerBtn.addEventListener('click', e => {
     burgerBtn.classList.toggle('burger--active');
     navEl.classList.toggle('header__nav--active');
+})
+
+document.addEventListener('scroll', () => {
+    const scrollTop = window.pageYOffset;
+    if (scrollTop >= maxScroll) {
+        headerEl.classList.add('header--scrolled');
+    } else {
+        headerEl.classList.remove('header--scrolled');
+    }
 })
